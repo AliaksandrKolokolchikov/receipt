4e4fb13855a606f7073ea1bb810d90cc7ed75290
alexanderpoge <flekssando@mail.ru>
2024-11-21 22:31:43 +0300

    finish: test for HaveIngredients

---
+++ src/components/HaveIngredients/HaveIngredients.test.tsx
@@ -0,0 +1,74 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { HaveIngredients } from './HaveIngredients';
+import { fetchIngredients } from '../../store/haveIngredients';
+
+jest.mock('../../store/haveIngredients', () => ({
+  fetchIngredients: jest
+    .fn()
+    .mockImplementation((ingredients) => Promise.resolve(ingredients)),
+}));
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn((selector) =>
+    selector({
+      haveIngredients: { recipes: [] },
+    }),
+  ),
+  useDispatch: jest.fn(() => jest.fn()),
+}));
+
+const mockStore = configureStore({
+  reducer: {
+    haveIngredients: (state = { recipes: [] }) => state,
+  },
+});
+
+describe('HaveIngredients Component', () => {
+  it('renders and shows result after searching', () => {
+    render(
+      <Provider store={mockStore}>
+        <HaveIngredients />
+      </Provider>,
+    );
+
+    const searchInput = screen.getByPlaceholderText("What's cooking?");
+    fireEvent.change(searchInput, { target: { value: 'Sugar' } });
+    expect(searchInput).toHaveValue('Sugar');
+  });
+
+  it('renders button', () => {
+    render(
+      <Provider store={mockStore}>
+        <HaveIngredients />
+      </Provider>,
+    );
+
+    const showButton = screen.getByRole('button', { name: /show/i });
+    expect(showButton).toBeInTheDocument();
+
+    fireEvent.click(showButton);
+    expect(fetchIngredients).toHaveBeenCalledTimes(1);
+  });
+
+  it('dispatches fetchIngredients on button click', () => {
+    render(
+      <Provider store={mockStore}>
+        <HaveIngredients />
+      </Provider>,
+    );
+
+    const showButton = screen.getByRole('button', { name: /show/i });
+    const searchInput = screen.getByPlaceholderText("What's cooking?");
+
+    fireEvent.change(searchInput, { target: { value: 'Sugar' } });
+    fireEvent.click(showButton);
+
+    expect(fetchIngredients).toHaveBeenCalledWith('Sugar');
+    expect(fetchIngredients).toHaveBeenCalledTimes(2);
+  });
+});
